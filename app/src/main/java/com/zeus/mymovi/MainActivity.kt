3342c3e3fdd6ee4c89e2b4402a6190937c5a1dce
Zeus Cach <99676255+Passepartout22@users.noreply.github.com>
2022-05-10 18:26:29 -0500

    Principios con Jetpack Compuse y Layouts

---
+++ app/src/main/java/com/zeus/mymovi/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.zeus.mymovi
+
+import android.app.Application
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.zeus.mymovi.ui.theme.MyMoviTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyMoviTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background)
+                 {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(
+    showBackground = true,
+    name = "Android Greeting",
+    widthDp = 400,
+    heightDp = 200
+)
+@Composable
+fun DefaultPreview() {
+    MyMoviTheme {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceEvenly
+
+        ) {
+            Greeting("Zeus", modifier = Modifier.background(Color.LightGray))
+            Greeting("Android", modifier = Modifier.background(Color.Yellow))
+            Greeting("Zeus", modifier = Modifier.background(Color.Cyan))
+        }
+
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(text = "Hello $name!", modifier = modifier)
+}
